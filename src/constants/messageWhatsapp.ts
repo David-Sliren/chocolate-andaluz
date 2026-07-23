19bb3478f92335ccb1805d0291a24ca321b17e4f
David Sliren <bluee.97.berry@gmail.com>
2026-07-23 02:37:38 -0500

    feact(constants): agregar constante para los mensajes de whatssap

---
+++ src/constants/messageWhatsapp.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_MESSAGE = {
+  phoneNumber: "573173487915",
+  message:
+    "¡Hola, encuentro interesante la propuesta, me gustaria reservar un cupo para ir a la pasadia en la finca de chocolate andaluz!.",
+};
